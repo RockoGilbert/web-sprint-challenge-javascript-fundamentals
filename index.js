3387a994f731770c1cc9aaeb4de742b10da8f7a0
Rocko Gilbert <rocko2122@gmail.com>
2021-01-23 22:49:36 -0600

    Rocko-Gilbert

--- index.js
+++ index.js
@@ -28,8 +28,11 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+function summation(x) {
+  numbers [];
+  for(i = 0; i < add; i++) {
+    numbers.push(i+1);
+  }
 
   }
  
